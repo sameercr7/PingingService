d7cd3e835883d526f58e3c4d7c438e13a78d5911
sameercr7 <sameer05072001@gmail.com>
2024-05-24 14:11:43 +0530

    PingbackendFrontend Updated

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,48 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import './App.css';
+
+function App() {
+  const [statuses, setStatuses] = useState([]);
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    const fetchStatuses = async () => {
+      try {
+        setCounter(x => x + 1);
+        const response = await axios.get('http://localhost:3000/trigger-health-check');
+        const data = response.data;
+        setStatuses(data);
+      } catch (error) {
+        console.error('Error fetching statuses:', error);
+      }
+    };
+
+    fetchStatuses();
+    const intervalId = setInterval(fetchStatuses, 10000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  return (
+    <div className="bg-black">
+      <header className="App bg-black">
+        <h1 className="text-white">Pinging Service Dashboard</h1>
+        
+        <p className="text-white">Health check attempts: {counter}</p>
+        <div className="status-container">
+          {statuses.map((status, index) => (
+            <div
+              key={index}
+              className={`status-box ${status.status === 'UP' ? 'bg-green-500' : 'bg-red-500'}`}
+            >
+              <span className="text-white">{status.url}: {status.status}</span>
+            </div>
+          ))}
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ backend.js
@@ -0,0 +1,139 @@
+const express = require("express");
+const nodemailer = require("nodemailer");
+const http = require("http");
+const https = require("https");
+const cors = require("cors"); // Import cors
+const { URL } = require("url"); // Require the URL module
+const app = express();
+const dotenv = require("dotenv").config(); // Load environment variables
+app.use(cors());
+// Email configuration
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.GMAIL_USER, // Use environment variable
+    pass: process.env.GMAIL_PASS, // Use environment variable
+  },
+});
+
+// List of URLs to check
+const urls = [
+  "https://iwrdup.com/actuator/health",
+  "https://pragati.iwrdup.com/actuator/health",
+  "https://lmsupsdm.com/actuator/health",
+  "http://dmsupsdm.com/actuator/health",
+  "http://117.250.2.226:7070/actuator/health",
+  "http://117.250.2.226:6060/actuator/health",
+  // "http://178.238.227.191:7171/actuator/health",
+];
+
+// Interval in milliseconds (10 seconds)
+const interval = 60 * 1000;
+
+// Function to periodically check URL health
+const checkHealth = () => {
+  urls.forEach(async (url) => {
+    try {
+      const result = await checkUrl(url);
+      console.log(`Health check for ${url}:`, result);
+    } catch (error) {
+      console.error(`Error checking health for ${url}:`, error);
+    }
+  });
+};
+
+// Check URL health initially
+checkHealth();
+
+// Schedule health checks every 10 seconds
+setInterval(checkHealth, interval);
+
+// Endpoint to trigger health check
+app.get("/trigger-health-check", async (req, res) => {
+  try {
+    const results = await Promise.all(urls.map(checkUrl));
+    console.log("Health Check Results:", results);
+    res.json(results);
+  } catch (error) {
+    console.error("Error checking health:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+// Additional endpoint
+app.get("/hello", (req, res) => {
+  res.send("Hello, World!");
+});
+// jijfeoof
+// Function to check URL health
+async function checkUrl(url) {
+  const parsedUrl = new URL(url); // Parse the URL
+  const protocol = parsedUrl.protocol === "https:" ? https : http;
+
+  return new Promise((resolve, reject) => {
+    const requestOptions = {
+      hostname: parsedUrl.hostname,
+      port: parsedUrl.port || (parsedUrl.protocol === "https:" ? 443 : 80),
+      path: parsedUrl.pathname + parsedUrl.search,
+      method: "GET",
+      rejectUnauthorized: false,
+    };
+
+    const req = protocol.request(requestOptions, (response) => {
+      let data = "";
+
+      response.on("data", (chunk) => {
+        data += chunk;
+      });
+
+      response.on("end", () => {
+        const status = response.statusCode === 200 ? "UP" : "DOWN";
+        console.log("status",status);
+        const result = { url, status };
+        if (status === "DOWN" || status==="ERROR") {
+          // Send email notification
+          sendEmailNotification(url);
+        }
+       
+        resolve(result);
+      });
+    });
+  
+
+ 
+    req.on("error", (error) => {
+      console.error(`Error checking ${url}:`, error);
+      if (error.code === 'ETIMEDOUT') {
+        // Handle connection timeout error
+        sendEmailNotification(url);
+      }
+      resolve({ url, status: "ERROR" });
+    });
+
+    req.end();
+  });
+}
+
+// Function to send email notification
+function sendEmailNotification(url) {
+  const mailOptions = {
+    from: "no.reply.gccloud@gmail.com",
+    to: ["sameer.1923ec1111@kiet.edu","rahul@gccloudinfo.com","sameer@gccloudinfo.com"],
+    subject: "Server Down Email",
+    text: `The server at ${url} is down .`,
+  };
+
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.error("Error sending email:", error);
+    } else {
+      console.log("Email sent:", info.response);
+    }
+  });
+}
+
+// Start server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
